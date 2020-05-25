8ccb9f8089a18f3400d0c72ff6e653673e858f9f
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-05-25 23:52:36 +0000

    commit exercise 5 and 6, and add relations to tables

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,8 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jeremy", last_name: "Nasato", hourly_rate: 100)
+@store1.employees.create(first_name: "Jacklyn", last_name: "Harrietha", hourly_rate: 120)
+@store2.employees.create(first_name: "George", last_name: "St. Geegland", hourly_rate: 40)
+@store2.employees.create(first_name: "Gil", last_name: "Faizon", hourly_rate: 30)
